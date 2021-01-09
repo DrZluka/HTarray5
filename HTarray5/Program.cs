3ce13252e85fe9ce3adf7fc38e64feeef55e72fa
Федор-ПК\Федор <curafedor@gmail.com>
2021-01-09 14:57:48 +0200

    HTarray5

---
+++ HTarray5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HTarray5
+{
+    class Program
+    {
+        static void MaxValue(ref int[] array)
+        {
+            int maxValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+            }
+            Console.WriteLine(maxValue);
+            // ну, или просто: int maxValue = array.max();
+            //Console.WriteLine(maxValue);
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 8, 20, 1, 0, 55, 77, 22, 20, 21, 12 };
+
+            MaxValue(ref myArray);
+        }
+    }
+}
